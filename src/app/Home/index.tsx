f4fc3cf2dce30fb801993460b29e0ac284ee6de1
018medina <018.medina@gmail.com>
2025-11-13 11:38:10 -0300

    first commit

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,42 @@
+import { View, Image, TouchableOpacity, Text } from "react-native";
+
+import { Item } from "@/components/Item";
+import { Input } from "@/components/Input";
+import { Filter } from "@/components/Filter";
+import { Button } from "@/components/Button";
+
+import { styles } from "./styles";
+import { FilterStatus } from "@/types/FilterStatus";
+
+const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE];
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
+      <View style={styles.form}>
+        <Input placeholder="O que você precisa comprar?" />
+        <Button title="Entrar" activeOpacity={0.8} />
+      </View>
+
+      <View style={styles.content}>
+        <View style={styles.header}>
+          {FILTER_STATUS.map((status) => (
+            <Filter key={status} status={status} isActive />
+          ))}
+
+          <TouchableOpacity style={styles.clearButton}>
+            <Text style={styles.clearText}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
+
+        <Item
+          data={{ status: FilterStatus.DONE, description: "Café" }}
+          onStatus={() => console.log("mudar o status")}
+          onRemove={() => console.log("Remover")}
+        />
+      </View>
+    </View>
+  );
+}
